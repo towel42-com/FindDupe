b6e2341d238876d799ad54d96514863a9e0e66e8
scottaronbloom <scott@towel42.com>
2022-04-01 15:54:04 -0700

    Rev version

--- VersionInfo.cmake
+++ VersionInfo.cmake
@@ -21,7 +21,7 @@
 # SOFTWARE.
 
 SET( MAJOR_VERSION 1 )
-SET( MINOR_VERSION 1 )
+SET( MINOR_VERSION 2 )
 SET( APP_NAME "Find Dupe" )
 SET( VENDOR   "Scott Aron Bloom" )
 SET( HOMEPAGE "www.towel42.com" )
